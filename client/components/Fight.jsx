2d9023cf60e683986fa76c89b4c202e9b2b4930e
eq-hong <111675243+eq-hong@users.noreply.github.com>
2022-11-18 11:09:54 +1300

    Add loading button with onClick

--- client/components/Fight.jsx
+++ client/components/Fight.jsx
@@ -3,9 +3,31 @@ import { useSelector } from 'react-redux'
 import { Button, InputBase } from '@mantine/core'
 
 export default function Fight() {
+  const [boolean, setBoolean] = useState(false)
+
+  function handleClick() {
+    setBoolean(!boolean)
+    
+    // delay
+    let delayInMilliseconds = 1000; //1 second
+
+    setTimeout(function() {
+      //your code to be executed after 1 second
+    }, delayInMilliseconds);
+    // envoke the calculate winner function test thing
+    // make boolean button false again
+  }
+
   return (
     <div className="button-center">
-      <Button variant="light" color="pink" radius="xl" size="xl">
+      <Button
+        loading={boolean}
+        onClick={handleClick}
+        variant="light"
+        color="pink"
+        radius="xl"
+        size="xl"
+      >
         FIGHT
       </Button>
     </div>
